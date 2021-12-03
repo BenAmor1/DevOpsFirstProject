e06cd03b47d54bc5f49fc93090baad3758deb814
U-DESKTOP-BEFFA\mbenamor <mbenamor@DESKTOP-Beffa.3s.com.tn>
2021-12-03 14:35:16 +0100

    first commit

---
+++ src/main/java/tn/esprit/devops/DevOpsFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsFirstProjectApplication.class, args);
+	}
+
+}
